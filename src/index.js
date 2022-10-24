11c392d6442635f8a7ab3585c3e3a020817a068f
Arief897 <ariefmuhammad897@gmail.com>
2022-10-24 23:24:56 +0700

    end of lesson 14

--- src/index.js
+++ src/index.js
@@ -3,26 +3,21 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
-import { UserProvider } from './contexts/user.context';
-import { CategoriesProvider } from './contexts/categories.context';
-import { CartProvider } from './contexts/cart.context';
+import { Provider } from 'react-redux';
 
 import reportWebVitals from './reportWebVitals';
 
 import './index.scss';
+import { store } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-        <UserProvider>
-          <CategoriesProvider>
-            <CartProvider>
-              <App />
-            </CartProvider>
-          </CategoriesProvider>
-        </UserProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,19 +1,21 @@
-import { Fragment, useContext } from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Fragment } from "react";
+import { Outlet } from "react-router-dom";
+import { useSelector } from 'react-redux';
 
 import CartIcon from "../../component/cart-icon/cart-icon.component";
 import CartDropdown from "../../component/cart-dropdown/cart-dropdown.component";
 
+import {selectIsCartOpen} from "../../store/cart/cart.selector";
+import {selectCurrentUser} from "../../store/user/user.selector";
+
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { SignOutUser } from "../../utils/firebase/firebase.utils";
-import { UserContext } from "../../contexts/user.context";
-import { CartContext } from "../../contexts/cart.context";
 
-import { NavigationContainer, LogoContainer, NavLinks, NavLink } from "./navigation.styles";
+import { NavigationContainer, NavLinks, NavLink } from "./navigation.styles";
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
-  const { isCartOpen } = useContext(CartContext);
+  const currentUser = useSelector(selectCurrentUser);
+  const isCartOpen = useSelector(selectIsCartOpen);
   
     return (
       <Fragment>
