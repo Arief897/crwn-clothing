5a98390587944a70f4404c80cda3bf0280e8cd3c
Arief897 <ariefmuhammad897@gmail.com>
2022-10-25 12:13:46 +0700

    end of lesson 15

--- src/index.js
+++ src/index.js
@@ -5,18 +5,23 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 
+import { PersistGate } from 'redux-persist/integration/react';
+
+
 import reportWebVitals from './reportWebVitals';
 
 import './index.scss';
-import { store } from './store/store';
+import { store, persistor } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
           <App />
-      </BrowserRouter>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
